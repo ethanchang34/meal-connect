3c2ebf27a2ae217c4df183ae1aa432f4ed548cf7
Ethan <ethanchang34@yahoo.com>
2023-04-11 16:51:15 -0400

    styling

--- app/request/page.tsx
+++ app/request/page.tsx
@@ -10,19 +10,21 @@ export default async function Request() {
   }
   return (
     <main>
-      <h1 className="text-2xl font-bold">Request</h1>
-      <Link
-        href="/request/guest-pass"
-        className="bg-gray-700 text-white text-sm px-6 py-2 rounded-md"
-      >
-        Guest Pass
-      </Link>
-      <Link
-        href="/request/meal-exchange"
-        className="bg-gray-700 text-white text-sm px-6 py-2 rounded-md"
-      >
-        Meal Exchange
-      </Link>
+      <h1 className="text-2xl font-bold py-4 px-4">Request</h1>
+      <div id="parent" className="py-4 px-4 flex flex-col items-center">
+        <Link
+          href="/give/guest-pass"
+          className="bg-gray-700 text-white text-sm px-6 py-2 rounded-md w-40"
+        >
+          Guest Pass
+        </Link>
+        <Link
+          href="/give/meal-exchange"
+          className="bg-gray-700 text-white text-sm px-6 py-2 rounded-md w-40"
+        >
+          Meal Exchange
+        </Link>
+      </div>
     </main>
   );
 }
